37b26da21be4cf5607844fbd3c1a8d41a9c08260
Simone Pierazzini <s.pierazzini@geniodiligence.it>
2021-12-02 08:23:00 +0100

    day 2, second star

--- day2.hs
+++ day2.hs
@@ -6,6 +6,10 @@ data Direction = Forward Integer | Up Integer | Down Integer deriving (Eq, Show)
 
 data Position = Position Integer Integer deriving (Show)
 
+-- distance, depth, aim
+data Position3 = Position3 Integer Integer Integer
+
+-- data AnyPos = Position | Position3
 
 main = do
   contents <- readFile "day2.input"
@@ -19,12 +23,27 @@ main = do
 
   print r
 
+  let finalPos3 = move3(Position3 0 0 0, commands)
+
+  let r = depth3 finalPos3 * distance3 finalPos3
+
+  print r
+
 move :: (Position, [Direction]) -> Position
 
 move ( Position x y, Forward z:l ) = move(Position (x + z) y, l)
 move ( Position x y, Up z:l )      = move(Position x (y - z), l)
 move ( Position x y, Down z:l )    = move(Position x (y + z), l)
 move ( p, [] )                     = p
+
+
+move3 :: (Position3, [Direction]) -> Position3
+
+move3 ( Position3 dist depth aim, Forward x:l ) = move3(Position3 (dist + x) (depth + aim * x) aim, l)
+move3 ( Position3 dist depth aim, Down x:l ) = move3(Position3 dist depth (aim + x), l)
+move3 ( Position3 dist depth aim, Up x:l ) = move3(Position3 dist depth (aim - x), l)
+move3 ( p, [] ) = p
+
 
 parseCommands :: [String] -> [Direction]
 
@@ -34,11 +53,14 @@ parseCommands ("up":x:l)      = Up (toInt(x)) : parseCommands l
 parseCommands []              = []
 parseCommands _               = [] -- error "illegal input"
 
+
 toInt :: String -> Integer
 toInt = read
 
 depth :: Position -> Integer
 depth (Position _ z) = z
+depth3 (Position3 _ z _) = z
 
-distance :: Position -> Integer
+-- distance :: AnyPos -> Integer
 distance (Position d _) = d
+distance3 (Position3 d _ _) = d
