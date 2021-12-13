0568cab220db838ee7c59541eab0e055ec7b059c
Simone Pierazzini <s.pierazzini@geniodiligence.it>
2021-12-13 06:44:42 +0100

    day 12, second star

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 import Data.Char (isUpper)
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
-import Debug.Trace (trace)
+--import Debug.Trace (trace)
 import System.Environment (getArgs)
+import Data.List
+
+trace x y = y
 
 main :: IO ()
 main = do
@@ -13,16 +16,37 @@ main = do
 
   print $ solveDay12 contents
 
+  let x = solveDay12snd contents
+
+--  putStr $ map $ show x
+  --print $ x -- solveDay12snd contents
+  print $ length x
+--  print $ length $ nub x
+
+solveDay12snd contents =   allPaths2 ["start"] [] [] (parseGraph contents)
+
+allPaths2 :: [String] -> [String] -> [String] -> Map.Map String [String] -> [[String]]
+allPaths2 p@("end":rest) done done2 graph = trace ("done2:" ++ show p) [reverse p]
+allPaths2 p@(n: rest) done [] graph = res
+  where
+    reached = filter (\x -> x /= "start") (graph Map.! n)
+    snd = filter (not . bigCave) $ intersect reached done
+    fst = filter (\x -> x `notElem` done && x `notElem` snd) reached
+    newdone = if bigCave n then done else n : done
+    res = trace ("path2:" ++ show p ++ ", reached:" ++ show reached ++ ", fst:" ++ show fst ++ ", snd:"++show snd ++", newdone:"++show newdone ) concat ([allPaths2 (x : p) newdone [] graph | x <- fst] ++ [allPaths2 (x : p) newdone [x] graph | x <- snd])--
+allPaths2 p done x graph = trace ("jump-"++show x) allPaths p done graph
+    
+
 solveDay12 contents = length $ allPaths ["start"] [] (parseGraph contents)
 
 allPaths :: [String] -> [String] -> Map.Map String [String] -> [[String]]
 -- allPaths p@("end":rest) _ _ = p
-allPaths p@("end" : rest) done graph = [reverse p] -- trace ("done:" ++ show p)
+allPaths p@("end" : rest) done graph =trace ("done:" ++ show p) [reverse p] -- 
 allPaths p@(n : rest) done graph = res
   where
     reached = filter (`notElem` done) (graph Map.! n)
     newdone = if bigCave n then done else n : done
-    res = concat [allPaths (x : p) newdone graph | x <- reached] -- trace ("path:" ++ show p ++ ", reached:" ++ show reached ++ ", newdone:" ++ show newdone)
+    res = trace ("path:" ++ show p ++ ", reached:" ++ show reached ++ ", newdone:" ++ show newdone) concat [allPaths (x : p) newdone graph | x <- reached] -- 
 allPaths [] _ _ = []
 
 bigCave :: String -> Bool
